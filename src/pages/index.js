c98b6f6da9694a48c38855535cbba6d0c1c42b27
base-benji <bnyatome@usebase.io>
2026-04-05 16:20:58 -0500

    Restore index to render WelcomePage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import Layout from '../components/Layout'
 import WelcomePage from '../components/WelcomePage'
 
 export default function Index() {
-  return (
-    <div style={{ color: 'white', background: 'black', minHeight: '100vh' }}>test</div>
-  )
+  return <WelcomePage />
 }
+    <div style={{ color: 'white', background: 'black', minHeight: '100vh' }}>test</div>
